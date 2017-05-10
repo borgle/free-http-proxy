ad8bba2866175230aa471cf6d8165f6b4487890b
Yoker.Wu <Yoker.Wu@gmail.com>
2017-05-10 16:50:10 +0800

    remove unneeded line

--- monitor.sh
+++ monitor.sh
@@ -5,21 +5,18 @@ do
   # mysql
   count=`ps -ef | grep "mysqld" | grep -v "grep"`
   if [ "$?" != "0" ]; then
-    cd ~/service/
     echo '[mysqld] shutdown'
     systemctl restart mysqld
   fi
   # fetch
   count=`ps -ef | grep "python" | grep "service\.py fetch" | grep -v "grep"`
   if [ "$?" != "0" ]; then
-    cd /opt/proxy
     echo '[fetch] shutdown'
     nohup python service.py fetch >/dev/null 2>&1 &
   fi
   # check
   count=`ps -ef | grep "python" | grep "service\.py check" | grep -v "grep"`
   if [ "$?" != "0" ]; then
-    cd /opt/proxy
     echo '[check] shutdown'
     nohup python service.py check >/dev/null 2>&1 &
   fi
