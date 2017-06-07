8f0e588abccc04c43b8226e117509dbf4b2985cc
Yoker.Wu <Yoker.Wu@gmail.com>
2017-06-07 09:56:27 +0800

    add db scheme sql , create db user & grant rights

--- db.sql
+++ db.sql
@@ -1,3 +1,9 @@
+CREATE DATABASE `proxy` CHARACTER SET utf8mb4;
+CREATE USER 'proxy'@'%' IDENTIFIED BY 'proxy.Mysql.8'; 
+GRANT ALL PRIVILEGES  ON `proxy`.* TO 'proxy'@'%';
+FLUSH PRIVILEGES;
+
+USE `proxy`;
 CREATE TABLE `http` (
   `ip` varchar(35) DEFAULT NULL,
   `port` int(11) DEFAULT NULL,
@@ -5,4 +11,5 @@ CREATE TABLE `http` (
   `failtimes` int(11) DEFAULT '0',
   `svtime` datetime DEFAULT CURRENT_TIMESTAMP,
   UNIQUE KEY `idx_unique` (`ip`,`port`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4;
+
